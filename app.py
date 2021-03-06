5cbbae9c51173877f64e50502ef76b127118e2d1
Gaberoids <figueiropas@hotmail.com>
2021-03-06 23:33:17 +0000

    game.html created. Python for game page created. CSS file commented out the * and body selector

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import os
 from flask import Flask, render_template, flash, redirect, request, url_for, session
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId  # this will allow to use code to
 if os.path.exists('env.py'):
     import env
 
 
 app = Flask(__name__)
+
+# -------------- Database Connection -----------------------
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# creating an instance of pymongo. constructor method
+mongo = PyMongo(app)
+
 
 
 @app.route("/")
@@ -22,6 +33,50 @@ def description():
 def login():
     return render_template("login.html")
 
+# -------------- Game.html Python -----------------------
+
+@app.route("/game", methods=["GET", "POST"])
+def game():
+    player_l = list(mongo.db.players.find().sort("n_drinks", -1))
+    return render_template("game.html", players_list=player_l)
+
+@app.route("/add_player", methods=["GET", "POST"])
+def add_player():
+    if request.method == "POST":
+        new_player = {
+            "name": request.form.get("input_new_player"),
+            "n_drinks": request.form.get("input_n_drinks")
+            }
+        mongo.db.players.insert_one(new_player)
+        flash("New Player Added")
+    
+    return redirect(url_for("game"))
+
+
+@app.route("/delete_player/<player_id>")
+def delete_player(player_id):
+    mongo.db.players.remove({"_id": ObjectId(player_id)})
+    # if user removed, share the flash message
+    # removed_player = str(mongo.db.players.remove({"_id": ObjectId(player_id)})["name"])
+
+    flash("player has been removed ")
+    return redirect(url_for("game"))
+
+
+@app.route("/rank_update_refresh/<player_id>", methods=["GET", "POST"])
+def rank_update_refresh(player_id):
+
+    if request.method == "POST":
+        updated_n_drinks = {
+            "name": request.form.get("name_rank"),
+            "n_drinks": request.form.get("n_drinks_rank")
+        }
+        mongo.db.players.update({"_id": ObjectId(player_id)}, updated_n_drinks)
+        flash("Player number of drinks have been updated and rank has been refreshed")
+    
+    return redirect(url_for("game"))
+
+# -------------- END Game.html Python -----------------------
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
